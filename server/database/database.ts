6ced798a3bc22c289bd64bbbb9c1b6053e66ac2a
Indranil Halder <indranilhalder.dev@gmail.com>
2023-05-22 18:59:27 +0100

    Mongodb production url

--- server/database/database.ts
+++ server/database/database.ts
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 import config from "../config/config";
 
-const { MONGO_URI } = config;
+const { NODE_ENV, MONGO_URI, PROD_MONGO_URI } = config;
+
+const mongodbUri = NODE_ENV === "development" ? MONGO_URI : PROD_MONGO_URI;
 
 const connectDatabase: () => Promise<void> = async (): Promise<void> => {
   try {
-    const conn = await mongoose.connect(MONGO_URI);
+    const conn = await mongoose.connect(mongodbUri);
     console.log(
       `Mongodb Connected: ${conn.connections[0].host}`.cyan.underline
     );
